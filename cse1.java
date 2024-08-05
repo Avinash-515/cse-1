1c8fd06a651927da05caeace3858a168fa8887f0
Avinash-515 <sai990750@gmail.com>
2024-08-05 03:04:50 -0700

    Update cse1.java

--- cse1.java
+++ cse1.java
@@ -1,9 +1,5 @@
-// Java program to take an integer
-// as input and print it
 import java.io.*;
 import java.util.Scanner;
-
-// Driver Class
 class GFG {
 	// main function
 	public static void main(String[] args)
